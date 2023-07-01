696cdd5cca0e8470220469d7efce9fdf972be803
matsukif <100049736+matsukif@users.noreply.github.com>
2023-07-02 06:20:18 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import openai
+
+# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+
+system_prompt = """
+このスレッドでは以下ルールを厳格に守ってください。
+あなたはレストランの評判情報から以下の５点を整理して回答するシステムです。
+・レストランの概要
+・レストランの雰囲気
+・人気メニュー
+・接客態度
+・ペット店内可否
+
+レストランの評判情報は複数の人が記載したものを「改行」でつないで入力します。
+
+"""
+
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": system_prompt}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+#    messages = st.session_state["messages"]
+    
+    st.session_state["messages"] = [
+      {"role": "system", "content": system_prompt}
+      ]
+
+    messages = st.session_state["messages"]
+    
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    st.session_state["messages"].append(user_message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )  
+
+    bot_message = response["choices"][0]["message"]
+    messages.append(bot_message)
+
+    st.session_state["user_input"] = ""  # 入力欄を消去
+
+
+# ユーザーインターフェイスの構築
+st.title("レストランの口コミを要約します")
+#st.image("bom_v2.1.png")
+st.write("レストランの口コミを入力してください")
+st.write("概要、雰囲気、人気メニュー、接客態度、ペット店内可否の５つの観点で整理します")
+# st.write("例：Toyosu Building, 3-3-3 Toyosu, Koto-ku, Tokyo, Japan")
+
+user_input = st.text_input("レストランの口コミ", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        speaker = "＜口コミ情報＞"
+        if message["role"]=="assistant":
+            speaker="＜要約結果＞"
+
+        # st.write(speaker + ": " + message["content"])
+        st.write(speaker)
+        st.write(message["content"])
